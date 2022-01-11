98663f75fc935cf14e1f3ebb7a9075d80dc8cfda
Jerry.ju <Jerry.ju@everylink.ai>
2022-01-11 17:24:53 +0800

    手续费账户提现、跨链

--- watcher/vmchain-watcher-node/app/service/traverse_event.js
+++ watcher/vmchain-watcher-node/app/service/traverse_event.js
@@ -12,9 +12,16 @@ let WsProvider = http.WsProvider
 let ApiPromise = http.ApiPromise
 
 const wsProvider = new WsProvider("ws://10.233.65.230:9900");
-// let context = fs.readFileSync("E:\\project\\IdeaProject\\chainscan-infra\\watcher\\vmchain-watcher-node\\app\\service\\types.json");
-let context = fs.readFileSync("../../config/types.json");
+let context = fs.readFileSync("E:\\project\\IdeaProject\\chainscan-infra\\watcher\\vmchain-watcher-node\\app\\service\\types.json");
+// let context = fs.readFileSync("../../config/types.json");
 let typesData= JSON.parse(context);
+
+// http://xapi.powx.io/l2-server-jsrpc/jsrpc      rinkeby
+// http://xapi.powx.io/l2-server3-jsrpc/jsrpc     vm
+const L2Address = {
+    "rinkeby": "http://xapi.powx.io/l2-server-jsrpc/jsrpc",
+    "vm": "http://xapi.powx.io/l2-server3-jsrpc/jsrpc"
+}
 
 
 async function main () {
@@ -25,46 +32,89 @@ async function main () {
     });
     // Subscribe to system events via storage
     api.query.system.events((events) => {
-        console.log(`\nReceived ${events.length} events: ` );
         // Loop through the Vec<EventRecord>
-
-        events.forEach((record) => {
-            // Extract the phase, event and the event types
+        events.forEach(async (record) => {
+            // Extract the event
             let event = record.event;
-            const types = event.typeDef;
-            console.log(event.data);
+            if (event.section.toString() === "cposContribution" && event.method.toString() === "Dividend") {
+                const block_height = event.data[0].toString()
+                const reverse = parseInt(event.data[1].toString())
+                const timestamp = event.data[2].toString()
+                console.log(`Received dividend events, block height: ${block_height}, reverse: ${reverse}, timestamp: ${timestamp}`)
 
-            // http://xapi.powx.io/l2-server-jsrpc/jsrpc      rinkeby
-            // http://xapi.powx.io/l2-server3-jsrpc/jsrpc     vm
-            let address_array = ["http://xapi.powx.io/l2-server-jsrpc/jsrpc", "http://xapi.powx.io/l2-server3-jsrpc/jsrpc"]
-            address_array.forEach(async (address) => {
-                let web3Wallet = new web3("http://vmtest.infra.powx.io/v1/72f3a83ea86b41b191264bd16cbac2bf");
-                const ethWallet = new ethers.providers.Web3Provider(
-                    web3Wallet.eth.currentProvider
-                ).getSigner();
-                const syncProvider = await zksync.Provider.newHttpProvider(
-                    "http://xapi.powx.io/l2-server3-jsrpc/jsrpc", 1000
-                );
-                let signer = await zksync.Signer.fromPrivateKey("0f9390c5b10cb10befbedf8cf451bf16e4c1e70c80ec12051f5c65454bdb3707");
-                let signType = { verificationMethod: 'ECDSA', isSignedMsgPrefixed: false};
-                const syncWallet = await zksync.Wallet.fromEthSigner(ethWallet, syncProvider, signer, 1, signType);
-                const withdrawTransaction = await syncWallet.withdrawFromSyncToEthereum({
-                    ethAddress: "",
-                    token: "USDT",
-                    amount: ethers.utils.parseEther("1.0"),
-                    fee: ethers.utils.parseEther("0.001")
-                });
-                const transactionReceipt = await withdrawTransaction.awaitVerifyReceipt();
-                console.log(transactionReceipt)
-            })
+                let token = "USDT";
+                const type =  'Withdraw';
+
+                Object.keys(L2Address).map(async name => {
+                    if (name.trim() === "rinkeby"){
+                        // let amount = "50000000";
+                        let ethPrivateKey = "9d054bd9d4f13c37fea4daa1f2f96968ce272e5a8b455b0d516c09acbe2f2803";
+                        let web3Wallet = new web3("http://rinkeby.infra.powx.io/v1/72f3a83ea86b41b191264bd16cbac2bf");
+                        let provider = new ethers.providers.Web3Provider(
+                            web3Wallet.eth.currentProvider
+                        );
+                        const syncProvider = await zksync.Provider.newHttpProvider(
+                            L2Address[name], 1000
+                        );
+                        const ethWallet = new ethers.Wallet(ethPrivateKey,provider);
+                        const zkWallet =  await zksync.Wallet.fromEthSigner(ethWallet,syncProvider);
+
+                        // 查看 zkSync 账户余额
+                        let nonce = await zkWallet.getNonce()
+                        const state = await zkWallet.getAccountState();
+                        const committedBalances = state.committed.balances;
+                        const balance = committedBalances["USDT"];
+                        console.log(`current L2 name: ${name}, USDT balance: ${balance}`)
+
+                        const fee = await zkWallet.provider.getTransactionFee(type, zkWallet.address(), token);
+                        const withdrawTransaction = await zkWallet.withdrawFromSyncToEthereum({
+                            ethAddress:zkWallet.address(),
+                            token: token,
+                            amount: balance,
+                            fee:fee.totalFee,
+                            nonce:nonce
+                        });
+                        const transactionReceipt = await withdrawTransaction.awaitVerifyReceipt();
+                        await bridge(ethPrivateKey, balance)
+                    }
+                    else {
+                        // let amount = "500";
+                        let ethPrivateKey = "0f9390c5b10cb10befbedf8cf451bf16e4c1e70c80ec12051f5c65454bdb3707";
+                        let web3Wallet = new web3("http://vmtest.infra.powx.io/v1/72f3a83ea86b41b191264bd16cbac2bf");
+                        let provider = new ethers.providers.Web3Provider(
+                            web3Wallet.eth.currentProvider
+                        );
+                        const syncProvider = await zksync.Provider.newHttpProvider(
+                            L2Address[name], 1000
+                        );
+                        const ethWallet = new ethers.Wallet(ethPrivateKey,provider);
+                        const zkWallet =  await zksync.Wallet.fromEthSigner(ethWallet,syncProvider);
+
+                        // 查看 zkSync 账户余额
+                        const state = await zkWallet.getAccountState();
+                        const committedBalances = state.committed.balances;
+                        const balance = committedBalances["USDT"];
+                        console.log(`current L2 name: ${name}, USDT balance: ${balance}`)
+
+                        const fee = await zkWallet.provider.getTransactionFee(type, zkWallet.address(), token);
+                        const withdrawTransaction = await zkWallet.withdrawFromSyncToEthereum({
+                            ethAddress:zkWallet.address(),
+                            token: token,
+                            amount: balance,
+                            fee:fee.totalFee
+                        });
+                        const transactionReceipt = await withdrawTransaction.awaitVerifyReceipt();
+                    }
+                })
+            }
         });
     });
 }
 
-const bridge=async function(){
+const bridge = async function(){
+    // let ethPrivateKey = "9d054bd9d4f13c37fea4daa1f2f96968ce272e5a8b455b0d516c09acbe2f2803";
+    // let amount = "1000000";
     let wallet = new web3("http://rinkeby.infra.powx.io/v1/72f3a83ea86b41b191264bd16cbac2bf");
-    let ethPrivateKey = "d2302498102cb336f9291c9913031b11008a49d025b2e7b03f237371372a28e3";
-    let amount = "1000000";
     let token = "USDT";
     let decimals = 6;
     let provider = new ethers.providers.Web3Provider(
@@ -72,11 +122,8 @@ const bridge=async function(){
     );
 
     const ethWallet = new ethers.Wallet(ethPrivateKey,provider);
-
-    let contract= await bridgerCall.Call.createContractInstance(GlobalConstants.GlobalConstants.Contracts[token].from,GlobalConstants.GlobalConstants.ContractABIs.Erc20,wallet);
     let tx= await bridgerCall.Call.erc20Aprrove(GlobalConstants.GlobalConstants.Contracts[token].from,GlobalConstants.GlobalConstants.ETH_ERC20_HANDLER_ADDRESS,'150000','6',ethWallet);
     console.log(`${tx} 完成`);
-
     let txs =await bridgerCall.Call.erc20_despoit(
         GlobalConstants.GlobalConstants.ETH_BRIDGE_ADDRESS,
         token,
@@ -86,7 +133,6 @@ const bridge=async function(){
         bridgerCall.Call.getCallData(amount,decimals,ethWallet.address),
         ethWallet,
         decimals)
-
     console.log(`${txs} 完成`);
 
 }
