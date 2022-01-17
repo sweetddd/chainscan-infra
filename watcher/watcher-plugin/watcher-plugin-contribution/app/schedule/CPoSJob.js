b74ccd0a132cd47d9b65e846b599bd8b1ce07c20
henry <henry@everylink.ai>
2022-01-17 15:12:18 +0800

    初始提交

--- watcher/watcher-plugin/watcher-plugin-contribution/app/schedule/CPoSJob.js
+++ watcher/watcher-plugin/watcher-plugin-contribution/app/schedule/CPoSJob.js
@@ -28,7 +28,7 @@ const ethPrivateKey = feeAccountPrivateKey;
 module.exports = {
     schedule: {
         interval: '5s', // 1 分钟间隔
-        type: 'all', // 指定所有的 worker 都需要执行
+        type: 'worker', // 指定所有的 worker 都需要执行
     },
     async task(ctx) {
         console.log(cPosWatcher(ctx))
