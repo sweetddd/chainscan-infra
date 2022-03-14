680f216b1f90d73d296b1cef466b5516c287ca4b
Johnny.yin <Johnny.yin@everylink.ai>
2022-03-14 14:41:24 +0800

    fix:  修复扫块定时任务逻辑，入库逻辑

--- watcher/vmchain-watcher-node/app/schedule/vmchainWatcher.js
+++ watcher/vmchain-watcher-node/app/schedule/vmchainWatcher.js
@@ -9,6 +9,8 @@ const { hexToU8a, isHex, stringToU8a,hexToBn,numberToHex } =  require('@polkadot
 
 
 const vmWeb3Url = process.env.vmWeb3Url;
+//const vmWeb3Url = 'http://vmdev.infra.powx.io';
+//const vmWeb3Url = 'http://vmchain-dev-node-0-sandbox.chain-sandbox.svc.cluster.local:9934';
 const wsProvider = new HttpProvider(vmWeb3Url);
 let context = fs.readFileSync('./config/types.json');
 
@@ -30,45 +32,62 @@ module.exports = {
 };
 
 
-
+let next_schedule = true;
 
 async function scanBlock(ctx,api){
-
+    console.log('next_schedule')
+    console.log(next_schedule)
+    if(!next_schedule){
+        return
+    }
     let maxBlockNumber = await ctx.service.blocks.getMaxBlockNumber();
     if(!maxBlockNumber){
-        maxBlockNumber = 1;
+        maxBlockNumber=1;
     }
+    // console.log('maxBlockNumber')
+    // console.log('maxBlockNumber')
+    // console.log('maxBlockNumber')
+    // console.log('maxBlockNumber')
+    // console.log(maxBlockNumber)
     await baseBlock(ctx,api,maxBlockNumber);
     await baseBlock(ctx,api,maxBlockNumber+1);
 
 }
 
 async function baseBlock(ctx,api,maxBlockNumber){
-
-
+    next_schedule = false;
+    console.log(maxBlockNumber)
     let newBlock =  await api.query.cposContribution.contributionBlocks(maxBlockNumber);
 
     if(!newBlock.toString()){
         console.log("no mobi data")
     }else{
-        let newBlockJson = JSON.parse(newBlock.toString());
-        let blockTime = newBlockJson.start_time.toString();
+        try {
+            let newBlockJson = JSON.parse(newBlock.toString());
+            let blockTime = newBlockJson.start_time.toString();
 
 
-        if(blockTime.length == 10){
-            blockTime = blockTime +"000"
+            if(blockTime.length == 10){
+                blockTime = blockTime +"000"
+            }
+            let newTime = new Date(parseInt(blockTime));
+            if (blockTime == 0){
+                newTime = new Date();
+            }
+
+            newBlockJson.start_time = newTime;
+
+            await ctx.service.blocks.addBlock(newBlockJson,ctx);
+            for(let tx in newBlockJson.transactions){
+                await ctx.service.transactions.addTransactionsDetail(newBlockJson,newBlockJson.transactions[tx]);
+            }
+            next_schedule = true;
+            
+        } catch (error) {
+            console.log(error.message);
+            baseBlock(ctx,api,maxBlockNumber)
         }
-        let newTime = new Date(parseInt(blockTime));
-        if (blockTime == 0){
-            newTime = new Date();
-        }
-
-        newBlockJson.start_time = newTime;
-
-        await ctx.service.blocks.addBlock(newBlockJson,ctx);
-        for(let tx in newBlockJson.transactions){
-            await ctx.service.transactions.addTransactionsDetail(newBlockJson,newBlockJson.transactions[tx]);
-        }
+        
     }
 
 }
--- watcher/vmchain-watcher-node/app/service/transactions.js
+++ watcher/vmchain-watcher-node/app/service/transactions.js
@@ -1,10 +1,11 @@
-'use strict';
+"use strict";
 
-const _ = require('lodash');
-const Service = require('egg').Service;
-const TABLE = 'transaction';
-const { env } = require('process');
-
+const _ = require("lodash");
+const Service = require("egg").Service;
+const TABLE = "transaction";
+const { env } = require("process");
+const web3 = require("web3");
+const { hexToStr } = require("../utils");
 
 class TransactionsService extends Service {
   constructor(ctx) {
@@ -12,16 +13,20 @@ class TransactionsService extends Service {
     this.database = this.app.mysql;
   }
 
-
   async getTransactionByHash(hash) {
-    const res = await this.database.query(`select  * from  ${TABLE} where  transaction_hash = ?`,[hash]);
+    const res = await this.database.query(
+      `select  * from  ${TABLE} where  transaction_hash = ?`,
+      [hash]
+    );
     return res;
   }
 
-
   hexToStr(hex, encoding) {
     let trimedStr = hex.trim();
-    let rawStr = trimedStr.substr(0, 2).toLowerCase() === "0x" ? trimedStr.substr(2) : trimedStr;
+    let rawStr =
+      trimedStr.substr(0, 2).toLowerCase() === "0x"
+        ? trimedStr.substr(2)
+        : trimedStr;
     let len = rawStr.length;
     if (len % 2 !== 0) {
       console.log("Illegal Format ASCII Code!");
@@ -39,21 +44,20 @@ class TransactionsService extends Service {
     return str;
   }
 
-  async sumVolume(startHeight ,endHeight) {
-    const res = await this.database.query(`select  sum(value) from  ${TABLE} where chain_type = 'CPoS' and block_number >= ? and block_number <= ?`,[startHeight,endHeight]);
+  async sumVolume(startHeight, endHeight) {
+    const res = await this.database.query(
+      `select  sum(value) from  ${TABLE} where chain_type = 'CPoS' and block_number >= ? and block_number <= ?`,
+      [startHeight, endHeight]
+    );
     return res;
   }
 
-
-
-
   /**
-     * 增加transaction详情
-     * @param transaction_hash transaction hash
-     * @return
-     */
-  async addTransactionsDetail(block,tx) {
-
+   * 增加transaction详情
+   * @param transaction_hash transaction hash
+   * @return
+   */
+  async addTransactionsDetail(block, tx) {
     // let exitTransaction =  await this.getTransactionByHash(tx.transaction_hash);
     // if(exitTransaction[0]) {
     //   return;
@@ -61,23 +65,45 @@ class TransactionsService extends Service {
     //
     // }
 
-    const addTxSql = 'INSERT INTO transaction (' +
-        'transaction_hash,block_hash,block_number,tx_timestamp,from_addr,to_addr,value,coin_symbol,price,buyer_fee,seller_fee,amount,chain_type' +
-        ') VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)';
+    const addTxSql =
+      "INSERT INTO transaction (" +
+      "transaction_hash,block_hash,block_number,tx_timestamp,from_addr,to_addr,value,coin_symbol,price,buyer_fee,seller_fee,amount,chain_type" +
+      ") VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
     let time = tx.transaction_time.toString();
-    if(time.length == 10){
+    if (time.length == 10) {
       time = time + "000";
     }
     let newTime = new Date(time);
     tx.transaction_time = newTime;
-    const addTxSql_Params = [ tx.transaction_hash,block.block_hash, block.block_height,tx.transaction_time,tx.buyer_address,tx.seller_address,tx.transaction_volume,this.hexToStr(tx.coin_symbol),tx.price,tx.buyer_fee,tx.seller_fee,tx.amount,'CPoS'];
-    await this.app.mysql.query(addTxSql, addTxSql_Params, function(err, result) {
-      if (err) {
-        console.log('[INSERT SUBSCABTX ERROR] - ', err.message);
-        return;
-      }
-      console.log('INSERT ID:', result);
-    });
+    tx.amount = web3.utils.hexToNumberString(tx.amount);
+    const addTxSql_Params = [
+      tx.transaction_hash,
+      block.block_hash,
+      block.block_height,
+      tx.transaction_time,
+      tx.buyer_address,
+      tx.seller_address,
+      tx.transaction_volume,
+      this.hexToStr(tx.coin_symbol),
+      tx.price,
+      tx.buyer_fee,
+      tx.seller_fee,
+      tx.amount,
+      "CPoS",
+    ];
+    try {
+      await this.app.mysql.query(
+        addTxSql,
+        addTxSql_Params,
+        function (err, result) {
+          if (err) {
+            console.log("[INSERT SUBSCABTX ERROR] - ", err.message);
+            return;
+          }
+          console.log("INSERT ID:", result);
+        }
+      );
+    } catch (error) {}
   }
 }
 
