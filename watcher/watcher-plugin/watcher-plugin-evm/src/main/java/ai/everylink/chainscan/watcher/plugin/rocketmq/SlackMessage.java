7debbbd5b4eee8298bbfae50cb5b2e3ce37c0911
张辉 <david.zhang@everylink.ai>
2022-03-14 19:15:24 +0800

    [test]watcher对接slack通知

---
+++ watcher/watcher-plugin/watcher-plugin-evm/src/main/java/ai/everylink/chainscan/watcher/plugin/rocketmq/SlackMessage.java
@@ -0,0 +1,37 @@
+package ai.everylink.chainscan.watcher.plugin.rocketmq;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Map;
+
+@Data
+@ToString
+class SlackMessage {
+  /**
+   * Message id。消息唯一ID，每个消息的id必须不同。可以使用UUID，也可以使用System.nanoTime等，只要保证全局唯一就行。
+  */
+  private String id;
+ 
+  /**
+   * Template name。固定传"DTX_WATCHER_ALERT_TMPL"
+  */
+  private String template = "DTX_WATCHER_ALERT_TMPL";
+ 
+  /**
+   * Notification channel.固定传数字4
+  */
+  private Integer channel = 4;
+ 
+  /**
+   * Location language。固定传zh_CN
+  */
+  private String language = "zh_CN";
+ 
+  /**
+   * Template parameters。消息内容。必须包含2个key。
+   * 1. channelId - slack频道的id，参考上面的第2步【查找频道的ID】
+   * 2. msg ：要发送的消息内容，如"hello there"。可以发送MD格式字符，参考https://slack.com/intl/zh-tw/help/articles/202288908-   %E8%A8%AD%E5%AE%9A%E8%A8%8A%E6%81%AF%E6%A0%BC%E5%BC%8F
+   */
+  private Map<String, Object> parameters;
+}
---
+++ watcher/watcher-plugin/watcher-plugin-evm/src/main/java/ai/everylink/chainscan/watcher/plugin/rocketmq/SlackUtils.java
@@ -0,0 +1,77 @@
+package ai.everylink.chainscan.watcher.plugin.rocketmq;
+
+import ai.everylink.chainscan.watcher.core.util.SpringApplicationUtils;
+import ai.everylink.chainscan.watcher.plugin.config.EvmConfig;
+import com.google.common.collect.Maps;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import com.alibaba.fastjson.JSON;
+import org.springframework.util.StringUtils;
+
+/**
+ * SlackUtils
+ *
+ * @author: david.zhanghui@everylink.ai
+ */
+public final class SlackUtils {
+
+    private static Logger logger = LoggerFactory.getLogger(SlackUtils.class);
+
+    private SlackUtils() {
+    }
+
+    public static void sendSlackNotify(String channelId, String title, String msg) {
+        try {
+            DefaultMQProducer producer = new DefaultMQProducer("producer_group_dtx_alert");
+            producer.setNamesrvAddr(getNamesrvAddr());
+            producer.start();
+
+            //Create a message instance, specifying topic, tag and message body.
+            Message rocketMsg = new Message("notification-normal", "dtx_alert",
+                    JSON.toJSONString(buildSlackMessage(channelId, title, msg)).getBytes(StandardCharsets.UTF_8));
+
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(rocketMsg);
+            logger.info("[slack]sendResult={}", sendResult);
+
+            //Shut down once the producer instance is not longer in use.
+            producer.shutdown();
+        } catch (Exception e) {
+            logger.error("[slack]send notification fail.", e);
+        }
+    }
+
+    public static String getNamesrvAddr() {
+        String val = System.getenv("watcher.rocketmq.server.addr");
+        if (!StringUtils.isEmpty(val)) {
+            logger.info("[slack]got name server addr from env: {}", val);
+            return val;
+        }
+
+        val = SpringApplicationUtils.getBean(EvmConfig.class).getRocketmqSrvAddr();
+        logger.info("[slack]got name server addr from config: {}", val);
+        return val;
+    }
+
+    public static SlackMessage buildSlackMessage(String channelId, String title, String alertMsg) {
+        Map<String, Object> params = Maps.newHashMap();
+        params.put("channelId", channelId);
+        params.put("title", title);
+        params.put("msg", alertMsg);
+
+        SlackMessage bean = new SlackMessage();
+        bean.setId(UUID.randomUUID().toString());
+        bean.setParameters(params);
+
+        return bean;
+    }
+
+}
