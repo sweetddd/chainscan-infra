121810de8b85aa76da920192ab51f4b3d03b12ab
Jerry.ju <Jerry.ju@everylink.ai>
2022-08-02 12:03:10 +0800

    修改报警信息

--- watcher/watcher-plugin/watcher-plugin-evm/src/main/java/ai/everylink/chainscan/watcher/plugin/ChainMonitorWatcher.java
+++ watcher/watcher-plugin/watcher-plugin-evm/src/main/java/ai/everylink/chainscan/watcher/plugin/ChainMonitorWatcher.java
@@ -155,7 +155,7 @@ public class ChainMonitorWatcher implements IWatcher {
             if (diff < WatcherUtils.getChainMonitorThreshold() * 60 * 1000) {
                 return;
             }
-            String chainName = chainId == 4 || chainId == 230 ? "Rinkeby": "DTX";
+            String chainName = (chainId == 4) ? "Rinkeby": "DTX";
             SlackUtils.sendSlackNotify("C02SQNUGEAU", chainName + "链告警",
                     chainName + "链长时间未出块，请关注！最后出块于(\"" + diff / 1000 / 60 + "\")分钟前");
         } catch (Exception e) {
