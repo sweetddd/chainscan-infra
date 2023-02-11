1092c83abc7a6990f88b664a15209cb4ceccd25f
alan.chen <Alan.chen@everylink.ai>
2023-02-11 17:13:13 +0800

    1

--- watcher/watcher-plugin/watcher-plugin-NFT-auction/src/main/java/ai/everylink/chainscan/watcher/plugin/strategy/ErcNftService.java
+++ watcher/watcher-plugin/watcher-plugin-NFT-auction/src/main/java/ai/everylink/chainscan/watcher/plugin/strategy/ErcNftService.java
@@ -1,32 +1,4 @@
 package ai.everylink.chainscan.watcher.plugin.strategy;
 
-import ai.everylink.chainscan.watcher.entity.NftAccount;
-import ai.everylink.chainscan.watcher.entity.NftAuction;
-import ai.everylink.chainscan.watcher.entity.Transaction;
-import ai.everylink.chainscan.watcher.plugin.bean.CreateNewNftAuctionBean;
-import org.web3j.protocol.Web3j;
-
-import java.math.BigInteger;
-import java.util.List;
-
-public interface ErcNftService {
-
-    ErcTypeNftEnum type();
-
-    boolean isScan();
-
-    String getNftData(Web3j web3j, String contract, BigInteger tokenId);
-
-    Long getNftId(List<String> params);
-
-    void createNewNftAuction(CreateNewNftAuctionBean createBean, NftAuction nftAuction);
-
-    void finishNftAuction(Transaction transaction, List<String> params);
-
-    void cancelNftAuction(Transaction transaction, List<String> params);
-
-    Long getAmount(Web3j web3j, String contractAddress, String address, Long tokenId);
-
-    int updateNftAccountAmount(NftAccount nftAccount, Long amount);
-
+public class ErcNftService {
 }
