40cfb6c4fa046663623d08f8c33d574050374c3e
alan.chen <Alan.chen@everylink.ai>
2023-06-05 12:02:11 +0800

    1.数据库链接数配置

--- watcher/watcher-core/src/main/java/ai/everylink/chainscan/watcher/core/config/DataSourceConfig.java
+++ watcher/watcher-core/src/main/java/ai/everylink/chainscan/watcher/core/config/DataSourceConfig.java
@@ -1,6 +1,9 @@
 package ai.everylink.chainscan.watcher.core.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -22,27 +25,52 @@ import java.util.Map;
 @EnableTransactionManagement
 @Configuration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@Slf4j
 public class DataSourceConfig {
+
+    @Value("${spring.datasource.hikari.maximum-pool-size}")
+    private int maximumPoolSize;
+    @Value("${spring.datasource.hikari.minimum-idle}")
+    private int minimumIdle;
+    @Value("${spring.datasource.hikari.max-lifetime}")
+    private long maxLifetime;
+    @Value("${spring.datasource.hikari.idle-timeout}")
+    private long idleTimeout;
 
     @Bean(name = "chainscanDataSource")
     @ConfigurationProperties(prefix = "spring.datasource.chainscan")
     public DataSource chainscanDataSource() {
-        DataSource build = DataSourceBuilder.create().build();
-        return build;
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setMaximumPoolSize(maximumPoolSize);
+        hikariDataSource.setMinimumIdle(minimumIdle);
+        hikariDataSource.setMaxLifetime(maxLifetime);
+        hikariDataSource.setIdleTimeout(idleTimeout);
+        log.info("chainscanDataSource:maximumPoolSize:{}, minimumIdle:{}, maxLifetime:{}, idleTimeout:{}", maximumPoolSize, minimumIdle, maxLifetime, idleTimeout);
+        return hikariDataSource;
     }
 
     @Bean(name = "walletDataSource")
     @ConfigurationProperties(prefix = "spring.datasource.wallet")
     public DataSource walletDataSource() {
-        DataSource build = DataSourceBuilder.create().build();
-        return build;
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setMaximumPoolSize(maximumPoolSize);
+        hikariDataSource.setMinimumIdle(minimumIdle);
+        hikariDataSource.setMaxLifetime(maxLifetime);
+        hikariDataSource.setIdleTimeout(idleTimeout);
+        log.info("walletDataSource:maximumPoolSize:{}, minimumIdle:{}, maxLifetime:{}, idleTimeout:{}", maximumPoolSize, minimumIdle, maxLifetime, idleTimeout);
+        return hikariDataSource;
     }
 
     @Bean(name = "marketplaceDataSource")
     @ConfigurationProperties(prefix = "spring.datasource.marketplace")
     public DataSource marketplaceDataSource() {
-        DataSource build = DataSourceBuilder.create().build();
-        return build;
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setMaximumPoolSize(maximumPoolSize);
+        hikariDataSource.setMinimumIdle(minimumIdle);
+        hikariDataSource.setMaxLifetime(maxLifetime);
+        hikariDataSource.setIdleTimeout(idleTimeout);
+        log.info("marketplaceDataSource:maximumPoolSize:{}, minimumIdle:{}, maxLifetime:{}, idleTimeout:{}", maximumPoolSize, minimumIdle, maxLifetime, idleTimeout);
+        return hikariDataSource;
     }
 
     @Bean
